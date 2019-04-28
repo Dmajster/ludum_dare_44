5160c6c5983453e0fa1c0c7ca246041a66e4bf4d
Primoz Pecar <pecar.primoz96@gmail.com>
2019-04-28 21:52:37 +0200

    gates working

---
+++ Assets/OpenHeaven.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Valve.VR.InteractionSystem.Sample {
+    public class OpenHeaven : MonoBehaviour {
+        public HoverButton hoverButton;
+        public GameObject GateLeft;
+        public GameObject GateRight;
+        public bool IsHeavenOpen = false;
+
+        private void Start() {
+            GateLeft.GetComponent<Animator>().enabled = false;
+            GateRight.GetComponent<Animator>().enabled = false;
+
+            if (hoverButton != null) {
+                hoverButton.onButtonDown.AddListener(OnButtonDown);
+            }
+        }
+
+        private void OnButtonDown(Hand hand) {
+            if (!IsHeavenOpen) {
+                GateLeft.GetComponent<Animator>().enabled = true;
+                GateRight.GetComponent<Animator>().enabled = true;
+
+                var animLeft = GateLeft.GetComponent<Animator>();
+                var animRight = GateRight.GetComponent<Animator>();
+
+                animRight.Play(0);
+                animLeft.Play(1);
+                IsHeavenOpen = true;
+            }
+        }
+
+    }
+}
