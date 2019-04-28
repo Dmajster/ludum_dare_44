a5b097064af460c645ec3a38ba7947000574c948
Primoz Pecar <pecar.primoz96@gmail.com>
2019-04-28 13:05:18 +0200

    Main  menu inpl and scene switching

---
+++ Assets/SwitchToGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Valve.VR.InteractionSystem.Sample {
+
+    public class SwitchToGame : MonoBehaviour {
+        public HoverButton hoverButton;
+
+        private void Start() {
+            if (hoverButton != null) {
+                hoverButton.onButtonDown.AddListener(OnButtonDown);
+            }
+        }
+
+        private void OnButtonDown(Hand hand) {
+            SceneManager.LoadScene(1);
+        }
+
+    }
+}
