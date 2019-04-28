41e235a9b6876520240c8746bad0c6244813262a
Primoz Pecar <pecar.primoz96@gmail.com>
2019-04-28 17:13:51 +0200

    crane wip

---
+++ Assets/ButtonControlCrane.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Valve.VR.InteractionSystem.Sample {
+    public class ButtonControlCrane : MonoBehaviour {
+        public HoverButton hoverButton;
+        public GameObject CraneTop;
+
+        private void Start() {
+            if (hoverButton != null) {
+                hoverButton.onButtonDown.AddListener(OnButtonDown);
+            }
+        }
+
+
+        private void OnButtonDown(Hand hand) {
+            var anim = CraneTop.GetComponent<Animator>();
+            anim.Play("crane_rotate");
+        }
+
+    }
+}
