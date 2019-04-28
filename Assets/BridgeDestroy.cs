a8328c0110408941cfde2e5d2e12ce435562f715
Domen Rostohar <domen.rostohar98@gmail.com>
2019-04-28 20:52:22 +0200

    added a destructible bridge

---
+++ Assets/BridgeDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets
+{
+    public class BridgeDestroy : MonoBehaviour
+    {
+        public GameObject PlankLeft;
+        public GameObject PlankRight;
+
+        public IEnumerator Start()
+        {
+            yield return new WaitForSeconds(5);
+            DestroyPlank();
+        }
+
+        public void DestroyPlank()
+        {
+            PlankLeft.transform.parent = null;
+            PlankRight.transform.parent = null;
+            PlankLeft.GetComponent<Rigidbody>().isKinematic = false;
+            PlankRight.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
